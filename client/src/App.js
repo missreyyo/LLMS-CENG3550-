095ab7ce06362909ad416e6bbad302c9f41d4fc0
missreyyo <reyhanduygu@posta.mu.edu.tr>
2024-01-02 14:32:49 +0300

    receiver address part added

--- client/src/App.js
+++ client/src/App.js
@@ -1,125 +1,152 @@
+import React, { useEffect, useState } from 'react';
 import Web3 from 'web3';
-import './App.css';
-
+import * as IpfsHttpClient from 'ipfs-http-client';
 
 function App() {
-    const contractAddress = '0x0405f1B0CE09dF8dc7De4187427b08c246F583aa'; // Replace with your deployed contract address
+  const [web3, setWeb3] = useState(null);
+  const [contract, setContract] = useState(null);
+  const [ipfs, setIpfs] = useState(null);
+  const [ipfsHash, setIpfsHash] = useState('');
+  const [receiverAddress, setReceiverAddress] = useState('');
+  const [contractHash, setContractHash] = useState('');
 
-    const abi = [
+  const contractAddress = '0xdCE5e8fAEE455Ae9929B031774b5c535B9c23b63'; 
+
+  const contractAbi = [
+    {
+      "inputs": [
         {
-            "inputs": [
-                {
-                    "internalType": "address",
-                    "name": "",
-                    "type": "address"
-                }
-            ],
-            "name": "dataEntries",
-            "outputs": [
-                {
-                    "internalType": "address",
-                    "name": "sender",
-                    "type": "address"
-                },
-                {
-                    "internalType": "string",
-                    "name": "ipfsHash",
-                    "type": "string"
-                }
-            ],
-            "stateMutability": "view",
-            "type": "function",
-            "constant": true
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "dataEntries",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "sender",
+          "type": "address"
         },
         {
-            "inputs": [
-                {
-                    "internalType": "string",
-                    "name": "_ipfsHash",
-                    "type": "string"
-                }
-            ],
-            "name": "setIPFSHash",
-            "outputs": [],
-            "stateMutability": "nonpayable",
-            "type": "function"
-        },
+          "internalType": "string",
+          "name": "ipfsHash",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
         {
-            "inputs": [
-                {
-                    "internalType": "address",
-                    "name": "_sender",
-                    "type": "address"
-                }
-            ],
-            "name": "getIPFSHash",
-            "outputs": [
-                {
-                    "internalType": "string",
-                    "name": "",
-                    "type": "string"
-                }
-            ],
-            "stateMutability": "view",
-            "type": "function",
-            "constant": true
+          "internalType": "string",
+          "name": "_ipfsHash",
+          "type": "string"
         }
-      ];
-
-    let web3;
-    let contract;
-    let ipfs;
-
-    window.onload = async () => {
-        // Connect to Metamask
-        if (window.ethereum) {
-            web3 = new Web3(window.ethereum);
-            try {
-                await window.ethereum.enable();
-                contract = new web3.eth.Contract(abi, contractAddress);
-
-                // Connect to IPFS
-                ipfs = createIPFSInstance();
-
-                displayContractHash();
-            } catch (error) {
-                console.error("User denied account access");
-            }
-        } else {
-            console.error("No web3 provider detected");
+      ],
+      "name": "setIPFSHash",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_sender",
+          "type": "address"
         }
+      ],
+      "name": "getIPFSHash",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    }
+  ];
+
+  useEffect(() => {
+    
+    const init = async () => {
+     
+      if (window.ethereum) {
+        const _web3 = new Web3(window.ethereum);
+        try {
+          await window.ethereum.enable();
+          setWeb3(_web3);
+
+          
+          const _contract = new _web3.eth.Contract(contractAbi, contractAddress);
+          setContract(_contract);
+
+          
+          const _ipfs = IpfsHttpClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https', headers: { authorization: '6008ad809050449eafff9403eba4b754' } });
+          setIpfs(_ipfs);
+
+         
+          displayContractHash();
+        } catch (error) {
+          console.error("User denied account access");
+        }
+      } else {
+        console.error("No web3 provider detected");
+      }
     };
 
-    function createIPFSInstance() {
-        const ipfs = window.IpfsHttpClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https', headers: { authorization: '1dab7f3ee54c42de8baa0cd86ae1cd3e' } });
-        return ipfs;
+    init();
+  }, []); 
+
+  const uploadToIPFS = async () => {
+    const textToUpload = prompt("Enter text to be uploaded to IPFS:");
+    const _receiverAddress = prompt("Enter receiver Ethereum address:");
+
+    if (textToUpload && _receiverAddress) {
+      setReceiverAddress(_receiverAddress);
+
+      
+      const ipfsResponse = await ipfs.add(textToUpload);
+      const _ipfsHash = ipfsResponse.cid.toString();
+      setIpfsHash(_ipfsHash);
+
+      
+      const accounts = await web3.eth.getAccounts();
+      await contract.methods.setIPFSHash(_receiverAddress, _ipfsHash).send({ from: accounts[0] });
+
+      displayContractHash();
+    } else {
+      console.error("Text or receiver address not provided");
     }
+  };
 
-    // eslint-disable-next-line 
-    async function uploadToIPFS() {
-        const textToUpload = document.getElementById("ipfsInput").value;
-
-        // Upload to IPFS
-        const ipfsResponse = await ipfs.add(textToUpload);
-        const ipfsHash = ipfsResponse.cid.toString();
-
-        // Update UI
-        document.getElementById("ipfsHash").innerText = ipfsHash;
-
-        // Set IPFS Hash in the Smart Contract
-        const accounts = await web3.eth.getAccounts();
-        await contract.methods.setIPFSHash(ipfsHash).send({ from: accounts[0] });
-
-        // Update UI with Contract IPFS Hash
-        displayContractHash();
+  const displayContractHash = async () => {
+    if (web3 && contract) {
+      const accounts = await web3.eth.getAccounts();
+      const _contractHash = await contract.methods.getIPFSHash(accounts[0]).call();
+      setContractHash(_contractHash);
     }
+  };
 
-    async function displayContractHash() {
-        const accounts = await web3.eth.getAccounts();
-        const contractHash = await contract.methods.getIPFSHash(accounts[0]).call();
-        document.getElementById("contractHash").innerText = contractHash;
-    }
-
+  return (
+    <div>
+      <h1>Ethereum IPFS Metamask Example</h1>
+      <div>
+        <button onClick={uploadToIPFS}>Upload to IPFS</button>
+      </div>
+      <div>
+        <p>IPFS Hash: {ipfsHash}</p>
+        <p>Receiver Ethereum Address: {receiverAddress}</p>
+        <p>Contract IPFS Hash: {contractHash}</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -4,21 +4,38 @@ pragma solidity ^0.8.0;
 contract SimpleStorage {
     struct DataEntry {
         address sender;
+        address recipient;
         string ipfsHash;
     }
 
-    mapping(address => DataEntry) public dataEntries;
+    mapping(address => DataEntry[]) public sentDataEntries;
+    mapping(address => DataEntry[]) public receivedDataEntries;
 
-    modifier onlySender() {
-        require(dataEntries[msg.sender].sender == msg.sender, "You are not the sender of this data entry");
+    modifier onlySender(address _recipient) {
+        require(
+            msg.sender == _recipient,
+            "You are not allowed to perform this action"
+        );
         _;
     }
 
-    function setIPFSHash(string memory _ipfsHash) public {
-        dataEntries[msg.sender] = DataEntry(msg.sender, _ipfsHash);
+    function setIPFSHash(address _recipient, string memory _ipfsHash) public onlySender(_recipient) {
+        DataEntry memory newDataEntry = DataEntry(msg.sender, _recipient, _ipfsHash);
+        sentDataEntries[msg.sender].push(newDataEntry);
+        receivedDataEntries[_recipient].push(newDataEntry);
     }
 
-    function getIPFSHash(address _sender) public view returns (string memory) {
-        return dataEntries[_sender].ipfsHash;
+    function getSentDataEntries() public view returns (DataEntry[] memory) {
+        return sentDataEntries[msg.sender];
+    }
+
+    function getReceivedDataEntries() public view returns (DataEntry[] memory) {
+        return receivedDataEntries[msg.sender];
+    }
+
+    function getIPFSHash(address _sender, address _recipient, uint256 _index) public view returns (string memory) {
+        require(_index < sentDataEntries[_sender].length, "Index out of bounds");
+        require(sentDataEntries[_sender][_index].recipient == _recipient, "Invalid recipient");
+        return sentDataEntries[_sender][_index].ipfsHash;
     }
 }
